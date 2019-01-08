2018bd4fc3ffb6936f84244300a59bf3c092e20b
blucky36 <blucky36@gmail.com>
2019-01-08 15:21:16 -0700

    initial commit

---
+++ src/components/cartItems.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import CartItem from "./cartItem.js"
+
+const CartItems = (props) => {
+  return (
+    <div className="container">
+      <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        {props.items.map((e)=><CartItem product = {e.product.name} price = {e.product.priceInCents} quantity = {e.quantity}/>)}
+      </div>
+    </div>
+  )
+}
+
+export default CartItems
---
+++ src/components/total.js
@@ -0,0 +1,31 @@
+import React from "react"
+import App from "../App.js"
+
+const formatValue = (cil) => {
+  let total = 0
+  cil.forEach((item)=>total+=item.product.priceInCents*item.quantity)
+  let arr = total.toString().split("")
+  return arr.map((e,i)=>{
+    if(i === 0){
+      return `$${e}`
+    }else if(i === arr.length - 2){
+      return `.${e}`
+    }else{
+      return e
+    }
+  }).join("")
+}
+
+const Total = (props) => {
+  return (
+    <div className="row">
+      <div className="col-2"></div>
+      <div className="col-8">
+        <span>{`Total Price: ${formatValue(props.items)}`}</span>
+      </div>
+      <div className = "col-2"></div>
+    </div>
+  )
+}
+
+export default Total
